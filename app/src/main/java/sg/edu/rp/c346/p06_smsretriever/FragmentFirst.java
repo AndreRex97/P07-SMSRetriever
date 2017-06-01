ec52c0660ff22f58915aa75f5c796be0a5be1979
15017103 <15017103@myrp.edu.sg>
2017-06-01 13:29:11 +0800

    initial commit

--- app/src/main/java/sg/edu/rp/c346/p06_smsretriever/FragmentFirst.java
+++ app/src/main/java/sg/edu/rp/c346/p06_smsretriever/FragmentFirst.java
@@ -6,13 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentFirst extends Fragment {
-
+    Button btnRetrieveSMS;
+    TextView tvSMS;
+    EditText etPhoneNumber;
 
     public FragmentFirst() {
         // Required empty public constructor
@@ -23,7 +28,13 @@ public class FragmentFirst extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_first, container, false);
+        View view =  inflater.inflate(R.layout.fragment_first, container, false);
+
+        tvSMS = (TextView) view.findViewById(R.id.tvSMS);
+        btnRetrieveSMS = (Button) view.findViewById(R.id.btnRetrieveSMS);
+        etPhoneNumber = (EditText) view.findViewById(R.id.etPhoneNumber);
+
+        return view;
     }
 
 }
--- app/src/main/java/sg/edu/rp/c346/p06_smsretriever/FragmentSecond.java
+++ app/src/main/java/sg/edu/rp/c346/p06_smsretriever/FragmentSecond.java
@@ -6,13 +6,18 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class FragmentSecond extends Fragment {
-
+    Button btnRetrieveSMS;
+    TextView tvSMS;
+    EditText etWord;
 
     public FragmentSecond() {
         // Required empty public constructor
@@ -23,7 +28,11 @@ public class FragmentSecond extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_second, container, false);
+        View view = inflater.inflate(R.layout.fragment_second, container, false);
+        tvSMS = (TextView) view.findViewById(R.id.tvSMS);
+        btnRetrieveSMS = (Button) view.findViewById(R.id.btnRetrieveSMS);
+        etWord = (EditText) view.findViewById(R.id.etWord);
+        return view;
     }
 
 }
