62c05189e97f249d53bf066a805891aa23a75321
15017103 <15017103@myrp.edu.sg>
2017-06-01 16:29:12 +0800

    initial commit

--- app/src/main/java/sg/edu/rp/c346/p06_smsretriever/FragmentSecond.java
+++ app/src/main/java/sg/edu/rp/c346/p06_smsretriever/FragmentSecond.java
@@ -1,14 +1,23 @@
 package sg.edu.rp.c346.p06_smsretriever;
 
 
+import android.Manifest;
+import android.content.ContentResolver;
+import android.content.pm.PackageManager;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.PermissionChecker;
+import android.text.format.DateFormat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -32,7 +41,71 @@ public class FragmentSecond extends Fragment {
         tvSMS = (TextView) view.findViewById(R.id.tvSMS);
         btnRetrieveSMS = (Button) view.findViewById(R.id.btnRetrieveSMS);
         etWord = (EditText) view.findViewById(R.id.etWord);
+
+        btnRetrieveSMS.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                int permissionCheck = PermissionChecker.checkSelfPermission
+                        (getActivity(), Manifest.permission.READ_SMS);
+
+                if (permissionCheck != PermissionChecker.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(getActivity(),
+                            new String[]{Manifest.permission.READ_SMS}, 0);
+                    return;
+                }
+
+                Uri uri = Uri.parse("content://sms");
+                String[] reqCols = new String[]{"date", "address", "body", "type"};
+                ContentResolver cr = getActivity().getContentResolver();
+
+//                // The filter String
+                String filter = "body LIKE ?";
+//                // the matches for the ?
+                String[] filterArgs = {"%" + etWord.getText().toString() + "%"};
+//                // Fetch SMS Message from Built-in Content Provider
+
+                Cursor cursor = cr.query(uri, reqCols, filter, filterArgs, null);
+                String smsBody = "";
+                if (cursor.moveToFirst()) {
+                    do {
+                        long dateInMillis = cursor.getLong(0);
+                        String date = (String) DateFormat
+                                .format("dd MMM yyyy h:mm:ss aa", dateInMillis);
+                        String address = cursor.getString(1);
+                        String body = cursor.getString(2);
+                        String type = cursor.getString(3);
+                        if (type.equalsIgnoreCase("1")) {
+                            type = "Inbox:";
+                        } else {
+                            type = "Sent:";
+                        }
+                        smsBody += type + " " + address + "\n at " + date + "\n\"" + body + "\"\n\n";
+                    } while (cursor.moveToNext());
+                }
+                tvSMS.setText(smsBody);
+            }
+        });
         return view;
     }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+
+        switch (requestCode) {
+            case 0: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    btnRetrieveSMS.performClick();
+                } else {
+                    Toast.makeText(getActivity(), "Permission not granted",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        }
+    }
 }
+
+
